2ce8919db3a1b413c8b35da05642fb664ae427ed
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2018-11-17 01:02:37 +0000

    getting started with groovy

---
+++ groovy/src/test/groovy/org/itrion/gdcr/FooTest.groovy
@@ -0,0 +1,12 @@
+package org.itrion.gdcr
+
+import spock.lang.Specification
+
+class FooTest extends Specification {
+    def 'foo test'() {
+        when:
+        1
+        then:
+        1 == 1
+    }
+}
