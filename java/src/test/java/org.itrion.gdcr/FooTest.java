2c0176bcf5c9db5616a3bcf1438b878abcad4a51
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2018-11-17 00:57:27 +0000

    getting started with java

---
+++ java/src/test/java/org.itrion.gdcr/FooTest.java
@@ -0,0 +1,12 @@
+package org.itrion.gdcr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FooTest {
+    @Test
+    void fooTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
