c13190e0309da54ee7052e0d5dd1e0cb51fc7c12
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2018-11-17 00:08:04 +0000

    getting started with kotlin

---
+++ Kotlin/src/main/kotlin/org.itrion.gdcr/Foo.kt
@@ -0,0 +1,5 @@
+package org.itrion.gdcr
+
+class Foo {
+
+}
---
+++ Kotlin/src/test/kotlin/org/itrion/gdcr/FooTest.kt
@@ -0,0 +1,13 @@
+package org.itrion.gdcr
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+internal class FooTest {
+    @Test
+    fun `foo test`() {
+        assertThat(1).isEqualTo(2)
+    }
+
+}
